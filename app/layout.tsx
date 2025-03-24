14c012cc41449bf1ce247e36520963207f4681f6
sangdo01 <91524163+sangdo01@users.noreply.github.com>
2025-03-24 16:23:54 +0700

    fix navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,13 +28,11 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <CartProvider>
-            {/* <div className="flex min-h-screen flex-col">
+            <div className="flex min-h-screen flex-col">
               <Navbar />
               <main className="flex-1">{children}</main>
               <Footer />
-            </div> */}
-
-            <div className="flex min-h-screen flex-col">{children}</div>
+            </div>
           </CartProvider>
         </ThemeProvider>
       </body>
